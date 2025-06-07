6b6109b8a89ec475a17d926c860971faf30539fa
hongvdp <hongvdp@gmail.com>
2025-06-07 08:52:43 +0700

    Revert "Initial"
    This reverts commit da2415a68d0b94a04cedd9f3c30bfef7c3899aa2.

--- django_ace/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Django-ace originally from https://github.com/django-ace/django-ace.
-"""
-
-from .widgets import AceWidget
--- martor/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Martor originally from https://github.com/agusmakmun/django-markdown-editor
-"""
-
-from .widgets import AdminMartorWidget, MartorWidget
